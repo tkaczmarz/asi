43dbc57db7a7a43ab95c94698bf39abae8437512
tkaczmarz <kaczmarzt@gmail.com>
2017-03-13 17:52:11 +0100

    Layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'sessions/new'
   root 'static_pages#home'
+  get '/home', to: 'static_pages#home'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
