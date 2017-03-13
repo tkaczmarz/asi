b8caa5d3b0ad0ed0213bc0e9395e39e8ae5097d2
tkaczmarz <kaczmarzt@gmail.com>
2017-03-13 14:08:03 +0100

    Logowanie

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'sessions/new'
   root 'users#index'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :users
 end
