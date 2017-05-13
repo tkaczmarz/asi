433e45a54ae8b110fbb41568b125b25b513b0175
tkaczmarz <kaczmarzt@gmail.com>
2017-05-13 15:25:28 +0200

    Naprawa bazy...

--- db/migrate/20170510150032_add_cars_company_foreign_keys.rb
+++ db/migrate/20170510150032_add_cars_company_foreign_keys.rb
@@ -1,8 +1,8 @@
 class AddCarsCompanyForeignKeys < ActiveRecord::Migration[5.0]
   def change
-  	add_foreign_key :cars, :companies
-  	add_foreign_key :companies, :cars
-  	add_foreign_key :companies, :users
-  	add_foreign_key :users, :companies
+  	#add_foreign_key :cars, :companies
+  	#add_foreign_key :companies, :cars
+  	#add_foreign_key :companies, :users
+  	#add_foreign_key :users, :companies
   end
 end
